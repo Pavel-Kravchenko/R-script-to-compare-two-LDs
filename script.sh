b91a419fa02abcbb64936ab4441cf72f94b76d6b
Pavel <30607315+Pavel-Kravchenko@users.noreply.github.com>
2018-03-29 08:24:28 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-echo "/Bash script run successfully/"
-name1=$1
-name2=$2
-echo "/R script run/"
-Rscript "/home/pavel/Desktop/Work/Project/R/R_2_plots/R_2_plots.R" --no-save --no-restore --args ${name1:3:(-4)} ${name2:3:(-4)} `pwd`
-echo "/Done/"
-echo "/Bash script end successfully/"
-
-
